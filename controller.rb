97c87af25e54f372ce996dbf6e89d85eff5f5f6d
Oona Singh <oonarsingh@gmail.com>
2023-01-30 16:10:13 +0000

    controller fixed for instance issue

--- controller.rb
+++ controller.rb
@@ -23,29 +23,37 @@ def run
       date = gets.chomp
       puts "Enter time block:"
       time_block = gets.chomp
-      schedule_time_block(date, time_block)
+      @productivity_app.schedule_time_block(date, time_block)
       puts "Congratulations. Time block scheduled."
     when 2
       puts "Enter date (YYYY-MM-DD)"
       date = gets.chomp
       puts "Add a habit:"
       habit = gets.chomp
-      add_habit(date, habit)
+      @productivity_app.add_habit(date, habit)
       puts "Great. That habit has been added for you."
     when 3
       puts "Enter date (YYYY-MM-DD)"
       date = gets.chomp
       puts "Track a habit:"
       habit = gets.chomp
-      track_habit(date, habit)
+      @productivity_app.track_habit(date, habit)
       puts "Awesome. You've tracked that habit."
     when 4
       puts "Enter date (YYYY-MM-DD)"
       date = gets.chomp
       puts "Add an actionable"
       actionable = gets.chomp
+      @productivity_app.add_actionable(date, actionable)
+      puts "Good. You've added an actionable to the day."
     when 5
+      puts "Enter date (YYYY-MM-DD)"
+      date = gets.chomp
+      puts "Retrieve actionables"
+      actionables = @productivity_app.retrieve_actionables(date)
+      puts "Actionables for #{date}: #{actionables}"
     when 6
+      puts "See ya!"
       break
     else puts "Whoops! That won't work."
     end
