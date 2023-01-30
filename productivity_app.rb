a40666c5f499790e3d7aae223e5fef3b535b0274
Oona Singh <oonarsingh@gmail.com>
2023-01-30 10:28:52 +0000

    initial commit

---
+++ productivity_app.rb
@@ -0,0 +1,27 @@
+class ProductivityApp
+  attr_accessor :data
+
+  def initialize
+    @data = {}
+  end
+
+  def schedule_time_block(date, time_block)
+
+  end
+
+  def add_habit(date, habit)
+
+  end
+
+  def track_habit(date, habit)
+
+  end
+
+  def add_actionable(date, actionable)
+
+  end
+
+  def retrieve_actionables(date)
+
+  end
+end
